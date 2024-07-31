3439c511b01a64dd275a68e92b1ea574a3d2be82
lmkha <lmkha0201@gmail.com>
2024-07-31 21:36:19 +0700

    finish HomeScreen UI, include add SwipeToDismissBox for NoteItem

--- app/src/main/java/com/example/mynote/composes/home/HomeScreen.kt
+++ app/src/main/java/com/example/mynote/composes/home/HomeScreen.kt
@@ -1,31 +1,42 @@
 package com.example.mynote.composes.home
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -34,9 +45,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -67,13 +81,13 @@ private fun HomeScreenContent(
     onNavigateToAddEditNote: () -> Unit,
     onSignOutNavigate: () -> Unit,
 ) {
-    val scrollBarBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     LaunchedEffect(uiState.isSignOut) {
         if (uiState.isSignOut) {
             onSignOutNavigate()
         }
     }
 
+    val scrollBarBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBarBehavior.nestedScrollConnection),
         topBar = {
@@ -85,10 +99,15 @@ private fun HomeScreenContent(
         bottomBar = {},
         floatingActionButton = {
             FloatingActionButton(
-                modifier = Modifier.padding(16.dp),
-                onClick = { onNavigateToAddEditNote() }
+                modifier = Modifier
+                    .padding(16.dp),
+                onClick = { onNavigateToAddEditNote() },
+                containerColor = Color(8, 135, 226),
+                contentColor = Color.White,
+                elevation = FloatingActionButtonDefaults.elevation(5.dp)
             ) {
                 Icon(
+                    modifier = Modifier.size(30.dp),
                     imageVector = Icons.Filled.Add,
                     contentDescription = null
                 )
@@ -184,38 +203,142 @@ private fun HomeScreenTopAppBar(
                 )
                 DropdownMenuItem(
                     text = { Text(text = "Manage account") },
-                    onClick = {  }
+                    onClick = { }
                 )
             }
         }
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
-private fun NoteItem(note: Note) {
-    Card(
+private fun NoteItem(
+    note: Note,
+    onSwipeStartToEnd: () -> Unit = {},
+    onSwipeEndToStart: () -> Unit = {},
+    onLongClick: () -> Unit = {},
+    onclick: () -> Unit = {},
+) {
+    val dismissState = rememberSwipeToDismissBoxState(
+        initialValue = SwipeToDismissBoxValue.Settled,
+        positionalThreshold = { it * 0.25f},
+        confirmValueChange = { state ->
+            when (state) {
+                SwipeToDismissBoxValue.StartToEnd -> {
+                    onSwipeStartToEnd()
+                    true
+                }
+
+                SwipeToDismissBoxValue.EndToStart -> {
+                    onSwipeEndToStart()
+                    true
+                }
+
+                else -> false
+            }
+        },
+
+    )
+
+    val cardHeight = 90.dp
+    val cardWidth = 350.dp
+    val cardPadding = PaddingValues(
+        top = 8.dp,
+        bottom = 8.dp,
+        start = 16.dp,
+        end = 16.dp
+    )
+    val cardShape = RoundedCornerShape(8.dp)
+
+    SwipeToDismissBox(
         modifier = Modifier
-            .height(90.dp)
-            .width(350.dp)
             .padding(
-                top = 8.dp,
-                bottom = 8.dp,
                 start = 16.dp,
-                end = 16.dp
-            ),
-        shape = RoundedCornerShape(8.dp),
+                end = 16.dp,
+            )
+            .clip(RoundedCornerShape(16.dp)),
+        state = dismissState,
+        backgroundContent = {
+            var alignment: Alignment.Horizontal = Alignment.Start
+            val color: Color
+            val icon: @Composable () -> Unit
+
+            when (dismissState.targetValue) {
+                SwipeToDismissBoxValue.StartToEnd -> {
+                    alignment = Alignment.Start
+                    color = Color.Green
+                    icon =
+                        { Icon(imageVector = Icons.Filled.Edit, contentDescription = "Edit wish") }
+                }
+
+                SwipeToDismissBoxValue.EndToStart -> {
+                    alignment = Alignment.End
+                    color = Color.Red
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Filled.Delete,
+                            contentDescription = "Delete wish"
+                        )
+                    }
+                }
+
+                else -> {
+                    color = Color.Transparent
+                    icon = {}
+                }
+            }
+
+            Card(
+                onClick = {  },
+                modifier = Modifier
+                    .height(cardHeight)
+                    .width(cardWidth)
+                    .padding(cardPadding)
+                    .clip(cardShape),
+                shape = cardShape,
+                colors = CardDefaults.cardColors(
+                    containerColor = color
+                )
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = alignment
+                ) {
+                    icon()
+                }
+            }
+        }
     ) {
-        Column(
-            modifier = Modifier.padding(8.dp)
+        Card(
+            modifier = Modifier
+                .height(cardHeight)
+                .width(cardWidth)
+                .padding(cardPadding)
+                .clip(cardShape)
+                .combinedClickable(
+                    onClick = { onclick() },
+                    onLongClick = { onLongClick() }
+                ),
+            shape = cardShape,
+            colors = CardDefaults.cardColors(
+                containerColor = Color(181, 223, 252)
+            ),
         ) {
-            Text(
-                text = note.title,
-                style = MaterialTheme.typography.titleMedium
-            )
-            Text(
-                text = note.content,
-                style = MaterialTheme.typography.bodyMedium
-            )
+            Column(
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text(
+                    text = note.title,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    text = note.content,
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
         }
     }
 }
@@ -224,23 +347,25 @@ private fun NoteItem(note: Note) {
 @Composable
 private fun HomeScreenPreview() {
     HomeScreenContent(
-//        uiState = HomeUiState(
-//            notes = listOf(
-//                Note(
-//                    title = "English certificate",
-//                    content = "Try to get 800"
-//                ),
-//                Note(
-//                    title = "Android Dev",
-//                    content = "Get a good job with 3000$ salary"
-//                ),
-//                Note(
-//                    title = "Air Blade",
-//                    content = "Buy a new one by the end of this year"
-//                ),
-//            ),
-//        ),
-        uiState = HomeUiState(),
+        uiState = HomeUiState(
+            notes = listOf(
+                Note(
+                    id = "1",
+                    title = "English certificate",
+                    content = "Try to get 800"
+                ),
+                Note(
+                    id = "2",
+                    title = "Android Dev",
+                    content = "Get a good job with 3000$ salary"
+                ),
+                Note(
+                    id = "3",
+                    title = "Air Blade",
+                    content = "Buy a new one by the end of this year buy a new one by the end of this year Buy a new one by the end of this year"
+                ),
+            ),
+        ),
         onSignOut = {},
         onNavigateToAddEditNote = {},
         onSignOutNavigate = {},
